ce176c29310faeac69c37f9e6ad823cd15139562
facundofabbi <facundofabbi@hotmail.com>
2021-06-10 22:49:02 -0300

    se agrego el precio y el boton para ver los insumos

--- src/routes/links-user.js
+++ src/routes/links-user.js
@@ -21,7 +21,7 @@ router.get("/ticketsJSON", hasPermission, async (req, res) => {
     const usuario = req.user
     id_usuario = usuario.id_usuario
     const aux = await pool.query(
-        "SELECT id_usuarioviaje,viaje,estado,cantidad FROM usuario_viaje WHERE usuario=?", [id_usuario]
+        "SELECT id_usuarioviaje,viaje,estado,cantidad,precio FROM usuario_viaje WHERE usuario=?", [id_usuario]
     );
     for (let i = 0; i < aux.length; i++) {
         viaje = await pool.query("SELECT ruta,fecha_salida,fecha_llegada,hora_salida,hora_llegada FROM viaje WHERE id_viaje=?", [aux[i].viaje]);
@@ -37,7 +37,6 @@ router.get("/ticketsJSON", hasPermission, async (req, res) => {
         aux[i].hora_llegada = hora_llegada
         const rutasViaje = await pool.query("SELECT r.origen AS origenid ,r.destino AS destinoid,r.id_ruta AS id_ruta,l.nombre AS nombreorigen,l2.nombre AS nombredestino FROM ruta r INNER JOIN lugar l ON (r.origen=l.id_lugar) INNER JOIN lugar l2 ON (r.destino=l2.id_lugar) WHERE id_ruta=?", [viaje[0].ruta]);
         aux[i].ruta = rutasViaje[0].nombreorigen + ' - ' + rutasViaje[0].nombredestino;
-        //aux[i].ruta = rutasViaje[0].nombreorigen + ' - ' + rutasViaje[0].nombredestino;
     }
 
     res.send(aux);
