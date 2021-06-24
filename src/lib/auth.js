3b66648926c6d060471680debe9b1f0429d7ccd8
facundofabbi <facundofabbi@hotmail.com>
2021-06-24 15:34:51 -0300

    Se agrego la funcionalidad isChofer

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -29,5 +29,19 @@ module.exports = {
             req.flash('warning', 'No puedes acceder a esta ruta!');
         }
         return res.redirect('/');
+    },
+
+    async isChofer(req, res, next) {
+        if (req.isAuthenticated()) {
+            const result = await pool.query('SELECT * FROM autoridad WHERE id_usuario=? AND rol=?', [req.user.id_usuario, ROLE.CHOFER]);
+            if (result.length > 0) {
+                return next();
+            } else {
+                req.flash('warning', 'No tienes los permisos para acceder a la ruta!');
+            }
+        } else {
+            req.flash('warning', 'No puedes acceder a esta ruta!');
+        }
+        return res.redirect('/');
     }
 }
